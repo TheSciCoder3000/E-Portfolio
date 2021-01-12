e8e09a8d27780dcd0b8aef1face7c077de60feed
TheSciCoder3000 <drjjdevilla2002@gmail.com>
2021-01-12 09:00:21 +0000

    server setting changes

--- e_portfolio/settings.py
+++ e_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5#bqlj&dy^6l(ubu+yc9v0dm5$gag!3+52+gq_j5yb$tlrx4&d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/EPort/E-Portfolio/static'
